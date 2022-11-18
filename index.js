e89cd9b9c46114b55c1980d2214bf88486dd6751
Iderlan/Yasmin <118554164+IderlanOliveira@users.noreply.github.com>
2022-11-18 14:23:06 -0300

    Criando python
    Iniciando o arquivo em python

--- index.js
+++ index.js
@@ -3,3 +3,7 @@ console.log("teste")
 
 let y = 0
 
+let x = 0
+
+console.log(x)
+
---
+++ index.py
@@ -0,0 +1,11 @@
+console.log("teste")
+
+let y = 0
+
+console.log(y)
+
+let z = y + 20 
+
+let o = 10
+
+console.log(o)
